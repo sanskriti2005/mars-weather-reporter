1bc4fc8b398d79227209643bb0b2d29c0881a8b9
Sanskriti <sanskritisri27@gmail.com>
2024-04-08 08:16:44 +0530

    added some started code to build the app upon

--- reporter.py
+++ reporter.py
@@ -13,7 +13,34 @@ def get_api_key():
 def read_user_arguments():
     #Argument Parser
     parser = argparse.ArgumentParser(
-        description="User asks for temperature and gets to know of the temperature in Mars."
+        description="Reports Mars' current atmospheric temperature"
     )
 
-    parser.add_argument
+    parser.add_argument(        #add arguments here
+
+    )
+
+
+    return parser.parse_args
+
+def building_url():
+    api_key = get_api_key()
+    url = (
+        f"<url>{api_key}<url>"
+    )
+    return url
+
+def get_data_from_url(url):
+    #create a request object for the built url and add a user-agent
+    req = request.Request(url, headers={'User-Agent':'Mozilla/5.0'})
+    
+
+
+def display_information(arg1, arg2=False, arg3=False):
+    print(f"Temp:{}")
+
+    if arg2:
+        print(f"")
+    if arg3:
+        print(f"")
+    
